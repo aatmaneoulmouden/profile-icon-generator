98efa5c770073de28a5582e2b34ee2f55278f9af
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-02-25 15:32:08 +0100

    Desktop demo: Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#2ecc71',
+        'secondary': '#3498db',
+        'light': '#ecf0f1',
+        'dark': '#2c3e50',
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    fontFamily: {
+      'montserrat': ['Montserrat', 'sans-serif'],
+    }
   },
   plugins: [],
 }
